13274dc4240c4f2193fc728068396a0d23f81a1b
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-02-08 22:09:04 -0600

    Using load grunt tasks

--- 2_Portfolio/grunt_workflow/Gruntfile.js
+++ 2_Portfolio/grunt_workflow/Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
     sass: {
