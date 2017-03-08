af753d06ead7243966697ba3b2e66ee028da9f5c
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-03-07 22:06:17 -0600

    Using jinja 2 templates

--- 3_Multi_User_Blog/Templates/templates.py
+++ 3_Multi_User_Blog/Templates/templates.py
@@ -1,14 +1,10 @@
 import os
+
+import jinja2
 import webapp2
 
-form_html = """
-<form>
-<h2>Add a Food</h2>
-<input type="text" name="food">
-%s
-<button>Add</button>
-</form>
-"""
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
 hidden_html = """
 <input type="hidden" name="food" value="%s">
@@ -30,6 +26,13 @@ shopping_list_html = """
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
 
 class MainPage(Handler):
     def get(self):
