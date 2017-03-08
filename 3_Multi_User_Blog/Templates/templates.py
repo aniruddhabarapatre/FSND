763254beac01bd9a9578c39b57f57a68ba346e3a
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-03-07 20:26:42 -0600

    Creating shopping list

--- 3_Multi_User_Blog/Templates/templates.py
+++ 3_Multi_User_Blog/Templates/templates.py
@@ -5,9 +5,26 @@ form_html = """
 <form>
 <h2>Add a Food</h2>
 <input type="text" name="food">
-<input type="hidden" name="food" value="eggs">
+%s
 <button>Add</button>
 </form>
+"""
+
+hidden_html = """
+<input type="hidden" name="food" value="%s">
+"""
+
+item_html = """
+<li>%s<li>
+"""
+
+shopping_list_html = """
+<br>
+<br>
+<h2>Shopping List</h2>
+<ul>
+%s
+</ul>
 """
 
 class Handler(webapp2.RequestHandler):
@@ -16,7 +33,22 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):
     def get(self):
-        self.write(form_html)
+        output = form_html
+        output_hidden = ""
+
+        # getting all items
+        items = self.request.get_all("food")
+        if items:
+            output_items = ""
+            for item in items:
+                output_hidden += hidden_html % item
+                output_items += item_html % item
+
+            output_shopping = shopping_list_html % output_items
+            output += output_shopping
+
+        output = output % output_hidden
+        self.write(output)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
