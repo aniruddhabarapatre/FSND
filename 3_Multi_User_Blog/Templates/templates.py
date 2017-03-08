004b88d753ba9315202ffe3c5f6569f6d7f14699
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-03-07 19:21:15 -0600

    Initial form setup

--- 3_Multi_User_Blog/Templates/templates.py
+++ 3_Multi_User_Blog/Templates/templates.py
@@ -1,13 +1,23 @@
 import os
 import webapp2
 
+form_html = """
+<form>
+<h2>Add a Food</h2>
+<input type="text" name="food">
+<input type="hidden" name="food" value="eggs">
+<button>Add</button>
+</form>
+"""
+
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-class MainPage(webapp2.RequestHandler):
+class MainPage(Handler):
     def get(self):
-        self.write("Hello Udacity")
+        self.write(form_html)
 
-app = webapp2.WSGIApplication([('/', MainPage)],
-                               debug=true)
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
