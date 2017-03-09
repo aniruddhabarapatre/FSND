0cd410d5c823bacc4c70d8c70ca07b1b0a331b7a
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-03-08 22:07:16 -0600

    Refactoring to use Jinja template

--- 3_Multi_User_Blog/Templates/templates.py
+++ 3_Multi_User_Blog/Templates/templates.py
@@ -6,23 +6,6 @@ import webapp2
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
-hidden_html = """
-<input type="hidden" name="food" value="%s">
-"""
-
-item_html = """
-<li>%s<li>
-"""
-
-shopping_list_html = """
-<br>
-<br>
-<h2>Shopping List</h2>
-<ul>
-%s
-</ul>
-"""
-
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
@@ -36,24 +19,9 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):
     def get(self):
-        self.render("shopping_list.html")
-        '''
-        output = form_html
-        output_hidden = ""
-
         # getting all items
         items = self.request.get_all("food")
-        if items:
-            output_items = ""
-            for item in items:
-                output_hidden += hidden_html % item
-                output_items += item_html % item
-
-            output_shopping = shopping_list_html % output_items
-            output += output_shopping
-
-        output = output % output_hidden
-        self.write(output)'''
+        self.render("shopping_list.html", items = items)
 
 class FizzBuzzHandler(Handler):
     def get(self):
