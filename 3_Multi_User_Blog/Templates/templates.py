82394c74636e55687b594d5471eb044ff4e235cb
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-03-09 18:57:15 -0600

    Using base template

--- 3_Multi_User_Blog/Templates/templates.py
+++ 3_Multi_User_Blog/Templates/templates.py
@@ -4,7 +4,7 @@ import jinja2
 import webapp2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
