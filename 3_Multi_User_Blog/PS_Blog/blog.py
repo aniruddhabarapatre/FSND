2b01ba0d7a54adab19fea3a0ff27354ba21e1a35
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-03-19 18:36:08 -0500

    Creating new post page and logic

--- 3_Multi_User_Blog/PS_Blog/blog.py
+++ 3_Multi_User_Blog/PS_Blog/blog.py
@@ -21,10 +21,27 @@ class Handler(webapp2.RequestHandler):
         self.write(self.render_str(template, **kw))
 
 class MainPage(Handler):
-
     def get(self):
         self.response.write("Udacity Blog!!!")
 
+class Newpost(Handler):
+    def render_newpost(self, subject="", content="", error=""):
+        self.render("newpost.html", subject=subject, content=content, error=error)
+
+    def get(self):
+        self.render_newpost()
+
+    def post(self):
+        subject = self.request.get("subject")
+        content = self.request.get("content")
+
+        if subject and content:
+            self.redirect("/")
+        else:
+            error = "We need both subject and content."
+            self.render_newpost(subject, content, error)
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/newpost', Newpost),
 ], debug=True)
