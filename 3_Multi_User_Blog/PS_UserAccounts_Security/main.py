1898a359b09d44961a886560b7605a30818e9bc5
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-04-02 10:34:54 -0500

    Refactoring signup logic

--- 3_Multi_User_Blog/PS_UserAccounts_Security/main.py
+++ 3_Multi_User_Blog/PS_UserAccounts_Security/main.py
@@ -1,7 +1,14 @@
 import os
+import re
+import random
+import hashlib
+import hmac
+from string import letters
 
-import jinja2
 import webapp2
+import jinja2
+
+from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
@@ -17,15 +24,54 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
 class Signup(Handler):
     def get(self):
-        self.render("signup.html")
+        self.render("signup-form.html")
 
     def post(self):
-        if error:
-            self.render("signup.html", errors)
+        have_error = False
+        self.username = self.request.get('username')
+        self.password = self.request.get('password')
+        self.verify = self.request.get('verify')
+        self.email = self.request.get('email')
+
+        params = dict(username = self.username,
+                      email = self.email)
+
+        if not valid_username(self.username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(self.password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif self.password != self.verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(self.email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('signup-form.html', **params)
         else:
-            self.redirect('/welcome?username=' + username)
+            self.done()
+
+    def done(self, *a, **kw):
+        raise NotImplementedError
 
 class Welcome(Handler):
     def get():
