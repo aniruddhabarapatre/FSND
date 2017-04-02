0fa2f03ea48ae74776d637d72572cfd60da1725f
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-04-01 21:48:13 -0500

    Hashing salt passwords

--- 3_Multi_User_Blog/UserAccounts_Security/hash_passwords.py
+++ 3_Multi_User_Blog/UserAccounts_Security/hash_passwords.py
@@ -1,11 +1,16 @@
 import random
 import string
-
-# implement the function make_salt() that returns a string of 5 random
-# letters use python's random module.
-# Note: The string package might be useful here.
+import hashlib
 
 def make_salt():
     return ''.join(random.choice(string.letters) for x in xrange(5))
 
-print make_salt()
+# implement the function make_pw_hash(name, pw) that returns a hashed password
+# of the format:
+# HASH(name + pw + salt),salt
+# use sha256
+
+def make_pw_hash(name, pw):
+    salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return "%s,%s" % (h, salt)
