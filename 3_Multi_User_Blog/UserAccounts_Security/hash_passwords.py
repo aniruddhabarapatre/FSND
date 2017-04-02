e1ee5f993dbb63254109c15752006fb990d05045
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-04-01 22:08:25 -0500

    validating passwords

--- 3_Multi_User_Blog/UserAccounts_Security/hash_passwords.py
+++ 3_Multi_User_Blog/UserAccounts_Security/hash_passwords.py
@@ -5,12 +5,18 @@ import hashlib
 def make_salt():
     return ''.join(random.choice(string.letters) for x in xrange(5))
 
-# implement the function make_pw_hash(name, pw) that returns a hashed password
-# of the format:
-# HASH(name + pw + salt),salt
-# use sha256
+# Implement the function valid_pw() that returns True if a user's password
+# matches its hash. You will need to modify make_pw_hash.
 
-def make_pw_hash(name, pw):
-    salt = make_salt()
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+        salt = make_salt()
     h = hashlib.sha256(name + pw + salt).hexdigest()
-    return "%s,%s" % (h, salt)
+    return '%s,%s' % (h, salt)
+
+def valid_pw(name, pw, h):
+    salt = h.split(",")[1]
+    return h == make_pw_hash(name, pw, salt)
+
+h = make_pw_hash('spez', 'hunter2')
+print valid_pw('spez', 'hunter2', h)
