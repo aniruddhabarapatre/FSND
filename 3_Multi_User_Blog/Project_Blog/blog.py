da65947859360049ddea4e5292c34d7fcbadbf33
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-04-02 21:16:40 -0500

    Initial structure for project blog

---
+++ 3_Multi_User_Blog/Project_Blog/blog.py
@@ -0,0 +1,31 @@
+import os
+import re
+from string import letters
+
+import jinja2
+import webapp2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+# Routes
+app = webapp2.WSGIApplication([
+
+], debug=True)
