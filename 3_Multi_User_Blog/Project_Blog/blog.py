43300a99e7460f99066afc50379d2b655d45d6fc
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-04-03 21:51:05 -0500

    Adding template and logic for permalink

--- 3_Multi_User_Blog/Project_Blog/blog.py
+++ 3_Multi_User_Blog/Project_Blog/blog.py
@@ -52,6 +52,17 @@ class Newpost(Handler):
             error = "We need both subject and content."
             self.render_newpost(subject, content, error)
 
+class PostPage(Handler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post=post)
+
 # Routes
 app = webapp2.WSGIApplication([
     ('/blog', BlogPage),
