a586a43dcedcafd46392b2f4fc15353e5bd30e58
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-04-06 20:37:42 -0500

    Adding signup and welcome page with logic

--- 3_Multi_User_Blog/Project_Blog/blog.py
+++ 3_Multi_User_Blog/Project_Blog/blog.py
@@ -1,9 +1,12 @@
 import os
 import re
+import random
+import hashlib
+import hmac
 from string import letters
 
-import jinja2
 import webapp2
+import jinja2
 
 from google.appengine.ext import db
 
@@ -25,6 +28,7 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+# Step 1: Basic Blog
 def blog_key(name = 'default'):
     return db.Key.from_path('blogs', name)
 
@@ -73,9 +77,77 @@ class PostPage(Handler):
 
         self.render("permalink.html", post=post)
 
+# Step 2: User Registration
+def users_key(group = 'default'):
+    return db.Key.from_path('users', group)
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+class Signup(Handler):
+    def get(self):
+        self.render("signup-form.html")
+
+    def post(self):
+        have_error = False
+        self.username = self.request.get('username')
+        self.password = self.request.get('password')
+        self.verify = self.request.get('verify')
+        self.email = self.request.get('email')
+
+        params = dict(username = self.username,
+                      email = self.email)
+
+        if not valid_username(self.username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(self.password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif self.password != self.verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(self.email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('signup-form.html', **params)
+        else:
+            self.done()
+
+    def done(self, *a, **kw):
+        raise NotImplementedError
+
+class Register(Signup):
+    def done(self):
+        #make sure the user doesn't already exist
+        u = User.by_name(self.username)
+        if u:
+            msg = 'That user already exists.'
+            self.render('signup-form.html', error_username = msg)
+        else:
+            u = User.register(self.username, self.password, self.email)
+            u.put()
+
+            self.login(u)
+            self.redirect('/welcome')
+
 # Routes
 app = webapp2.WSGIApplication([
     ('/blog', BlogPage),
     ('/blog/newpost', Newpost),
     ('/blog/([0-9]+)', PostPage),
+    ('/signup', Register),
 ], debug=True)
