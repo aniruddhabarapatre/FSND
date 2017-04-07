1291390acad9edfcd1ba699274f96023f600ab9b
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-04-06 22:32:21 -0500

    Creating password hash and salt

--- 3_Multi_User_Blog/Project_Blog/blog.py
+++ 3_Multi_User_Blog/Project_Blog/blog.py
@@ -14,9 +14,32 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                 autoescape = True)
 
-def render_str(template, **params):
+secret = "Udacity"
+
+def render_str(self, template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
+
+def make_secure_val(val):
+    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val
+
+def make_salt(length = 5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (salt, h)
+
+def valid_pw(name, password, h):
+    salt = h.split(',')[0]
+    return h == make_pw_hash(name, password, salt)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
