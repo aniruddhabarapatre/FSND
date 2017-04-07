945ba9631cffbcfccdc73c47b4d38164ea2e6c03
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-04-06 22:09:43 -0500

    Creating User class

--- 3_Multi_User_Blog/Project_Blog/blog.py
+++ 3_Multi_User_Blog/Project_Blog/blog.py
@@ -81,6 +81,27 @@ class PostPage(Handler):
 def users_key(group = 'default'):
     return db.Key.from_path('users', group)
 
+class User(db.Model):
+    name = db.StringProperty(required = True)
+    email = db.StringProperty()
+
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid, parent = users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email = None):
+        return User(parent = users_key(),
+                    name = name,
+                    pw_hash = pw_hash,
+                    email = email)
+
+
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
     return username and USER_RE.match(username)
