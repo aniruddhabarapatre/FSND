f2c1b84c6923b01455684b1c22e373c8281224ec
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-04-10 21:08:27 -0500

    Fixing user reference for post

--- 3_Multi_User_Blog/Project_Blog/blog.py
+++ 3_Multi_User_Blog/Project_Blog/blog.py
@@ -77,11 +77,44 @@ class Handler(webapp2.RequestHandler):
 def blog_key(name = 'default'):
     return db.Key.from_path('blogs', name)
 
+# moved here from Step 2 for referenceProperty
+def users_key(group = 'default'):
+    return db.Key.from_path('users', group)
+
+class User(db.Model):
+    name = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+    email = db.StringProperty()
+
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid, parent = users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email = None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent = users_key(),
+                    name = name,
+                    pw_hash = pw_hash,
+                    email = email)
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
+
 class Post(db.Model):
     subject = db.StringProperty(required = True)
     content = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
     last_modified = db.DateTimeProperty(auto_now_add = True)
+    user = db.ReferenceProperty(User, collection_name = "user_posts")
 
     def render(self):
         self._render_text = self.content.replace('\n', '<br>')
@@ -108,9 +141,10 @@ class Newpost(Handler):
 
         subject = self.request.get("subject")
         content = self.request.get("content")
+        user = User.by_name(self.user.name)
 
         if subject and content:
-            post = Post(parent=blog_key(), subject = subject, content = content)
+            post = Post(parent=blog_key(), subject = subject, content = content, user = user)
             post.put()
             self.redirect("/blog/%s" % str(post.key().id()))
         else:
@@ -193,37 +227,6 @@ class Likes(db.Model):
         return likes.count()
 
 # Step 2: User Registration
-def users_key(group = 'default'):
-    return db.Key.from_path('users', group)
-
-class User(db.Model):
-    name = db.StringProperty(required = True)
-    pw_hash = db.StringProperty(required = True)
-    email = db.StringProperty()
-
-    @classmethod
-    def by_id(cls, uid):
-        return User.get_by_id(uid, parent = users_key())
-
-    @classmethod
-    def by_name(cls, name):
-        u = User.all().filter('name =', name).get()
-        return u
-
-    @classmethod
-    def register(cls, name, pw, email = None):
-        pw_hash = make_pw_hash(name, pw)
-        return User(parent = users_key(),
-                    name = name,
-                    pw_hash = pw_hash,
-                    email = email)
-
-    @classmethod
-    def login(cls, name, pw):
-        u = cls.by_name(name)
-        if u and valid_pw(name, pw, u.pw_hash):
-            return u
-
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
