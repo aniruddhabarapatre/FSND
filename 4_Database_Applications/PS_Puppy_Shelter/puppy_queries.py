32ecfbd27926b7f6beeddaad26708129e86edcf4
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-05-07 22:12:21 -0500

    Get all puppies from last 6 months

--- 4_Database_Applications/PS_Puppy_Shelter/puppy_queries.py
+++ 4_Database_Applications/PS_Puppy_Shelter/puppy_queries.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import sessionmaker
 
 from puppies import Base, Shelter, Puppy
 
+import datetime
+
 engine = create_engine('sqlite:///puppyshelter.db')
 
 Base.metadata.bind = engine
@@ -18,4 +20,14 @@ def q1_all_puppies_asc_order():
     for puppy in results:
         print puppy[0]
 
-q1_all_puppies_asc_order()
+def q2_all_puppies_less_than_6months():
+    """Query all of the puppies that are less than 6 months old organized by the youngest first"""
+    today = datetime.date.today()
+    sixMonths = today - datetime.timedelta(days = 180)
+    results = session.query(Puppy.name, Puppy.dateOfBirth).filter(Puppy.dateOfBirth >= sixMonths).order_by(Puppy.dateOfBirth.desc())
+
+    for puppy in results:
+        print "{name}: {dob}".format(name=puppy[0], dob=puppy[1])
+
+#q1_all_puppies_asc_order()
+q2_all_puppies_less_than_6months()
