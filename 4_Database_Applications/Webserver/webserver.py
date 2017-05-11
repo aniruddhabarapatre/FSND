4bfe847121fab819f416c5fb9ca9911194cffdaa
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-05-10 21:41:05 -0500

    Creating a webserver

---
+++ 4_Database_Applications/Webserver/webserver.py
@@ -0,0 +1,32 @@
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import SocketServer
+
+class webserverHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>Hello!</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+        except IOError:
+            self.send_error(404, "File not found: %s" % self.path)
+
+def main():
+    try:
+        port = 8081
+        server = SocketServer.TCPServer(('', port), webserverHandler)
+        print "Webserver running on port %s" % port
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print "^C entered, stopping server"
+        server.socket.close()
+
+
+if __name__ == '__main__':
+        main()
