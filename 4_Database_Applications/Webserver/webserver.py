419e063b4465faf772a108e7ceac7eae897cc472
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-05-15 20:11:49 -0500

    creating another route

--- 4_Database_Applications/Webserver/webserver.py
+++ 4_Database_Applications/Webserver/webserver.py
@@ -14,6 +14,17 @@ class webserverHandler(SimpleHTTPRequestHandler):
                 self.wfile.write(output)
                 print output
                 return
+
+            if self.path.endswith("/hola"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>&#161Hola! <a href='/hello'>Back to Hello</a></body></html>"
+                self.wfile.write(output)
+                print output
+                return
         except IOError:
             self.send_error(404, "File not found: %s" % self.path)
 
