4e40ee3d8eeb26e43948e70285a9f485f0952333
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-05-17 22:21:39 -0500

    Adding database to application

--- 4_Database_Applications/Dev_with_Framework/flask_app.py
+++ 4_Database_Applications/Dev_with_Framework/flask_app.py
@@ -1,11 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 @app.route('/')
 @app.route('/hello')
 
 def HelloWorld():
-    return "Hello World!!!"
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '<br>'
+    return output
 
 
 if __name__ == '__main__':
