3d33236669fcd90bf15a6f57faa15babecc508fb
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-05-18 22:26:32 -0500

    Using templates for view

--- 4_Database_Applications/Dev_with_Framework/flask_app.py
+++ 4_Database_Applications/Dev_with_Framework/flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -18,15 +18,7 @@ session = DBSession()
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).first()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-    output = ''
-    for i in items:
-        output += i.name
-        output += '<br>'
-        output += i.price
-        output += '<br>'
-        output += i.description
-        output += '<br><br>'
-    return output
+    return render_template('menu.html', restaurant=restaurant, items=items)
 
 # Task 1: Create route for newMenuItem function here
 @app.route('/restaurants/<int:restaurant_id>/new/')
