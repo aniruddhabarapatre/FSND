a0ff5653f3c03444238b92023868de006e32565f
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2017-05-20 14:42:32 -0500

    Creating a menu item

--- 4_Database_Applications/Dev_with_Framework/flask_app.py
+++ 4_Database_Applications/Dev_with_Framework/flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
@@ -21,9 +21,15 @@ def restaurantMenu(restaurant_id):
     return render_template('menu.html', restaurant=restaurant, items=items)
 
 # Task 1: Create route for newMenuItem function here
-@app.route('/restaurants/<int:restaurant_id>/new/')
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == "POST":
+        newItem = MenuItem(name = request.form['name'], restaurant_id = restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 # Task 2: Create route for editMenuItem function here
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
